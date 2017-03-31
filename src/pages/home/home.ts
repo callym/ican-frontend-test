34dbdfb3c6b340b1d19dd20b30fb5b5485918ecc
Callym <hi@callym.com>
2017-03-31 01:08:07 +0100

    Added definitions and data for later

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,8 +8,67 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  title: String;
+  subtitle: String;
 
+  user: {
+    name: String,
+    date_of_birth: String,
+    gender: String,
+    nationality: String,
+    current_school: String,
+    current_major: String,
+    current_gpa: Number,
+    label: Array<String>,
+    lifestyle: Array<String>,
+  };
+
+  gender_data: Array<String>;
+  nationality_data: Array<String>;
+
+  label_data: Array<{
+    name: String,
+    added: Boolean
+  }>;
+
+  lifestyle_data: Array<{
+    name: String,
+    added: Boolean
+  }>;
+
+  constructor(public navCtrl: NavController) {
+    this.gender_data = ['Female', 'Male', 'Other'];
+    this.nationality_data = ['Chinese', 'English'];
+
+    this.label_data = [
+      { name: 'Outgoing', added: false },
+      { name: 'Reserved', added: false },
+      { name: 'Sporty', added: false },
+      { name: 'Routine', added: false },
+      { name: 'Flexible', added: false },
+      { name: 'Neutral', added: false }
+    ];
+
+    this.lifestyle_data = [
+      { name: 'Fast', added: false },
+      { name: 'Party', added: false },
+      { name: 'Quiet', added: false },
+      { name: 'Posh', added: false },
+      { name: 'Budget', added: false },
+      { name: 'Neutral', added: false }
+    ];
+
+    this.user = {
+      name: '',
+      date_of_birth: '',
+      gender: '',
+      nationality: '',
+      current_school: '',
+      current_major: '',
+      current_gpa: NaN,
+      label: [],
+      lifestyle: []
+    };
   }
 
 }
